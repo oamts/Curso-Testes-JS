a9ede2f79ebe6ee93f0ba97c624a81076dd2a773
mts-almeida <mateusoliveiraalmeida@gmail.com>
2023-01-09 22:29:24 -0300

    Aula 3 - Jest watch mode: observar alterações nos arquivos

--- Modulo1/Projeto1/src/lib/calculator.js
+++ Modulo1/Projeto1/src/lib/calculator.js
@@ -1,3 +1,8 @@
 module.exports.sum = (num1, num2) => {
-    return num1 + num2;
+    const n1 = parseInt(num1);
+    const n2 = parseInt(num2);
+    if(Number.isNaN(n1) || Number.isNaN(n2)) {
+        throw new Error('Please check your input');
+    }
+    return n1 + n2;
 }
