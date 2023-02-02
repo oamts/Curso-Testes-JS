2f5dcbd040cd11835173956076d7dae94524100f
mts-almeida <mateusoliveiraalmeida@gmail.com>
2023-02-01 21:45:16 -0300

    Aula 8 - Mais refactoring com suporte dos testes

--- Modulo1/Projeto1/src/lib/queryString.js
+++ Modulo1/Projeto1/src/lib/queryString.js
@@ -1,8 +1,8 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value))
+    throw new Error('please check you params');
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value))
-        throw new Error('please check you params');
-      return `${key}=${value}`;
-    })
-    .join('&');
+  Object.entries(obj).map(keyValueToString).join('&');
