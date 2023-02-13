8776c4cf8f7963479402c0460e141d3b287f6d92
mts-almeida <mateusoliveiraalmeida@gmail.com>
2023-02-12 21:46:14 -0300

    Aula 17 - Shopping Cart: Teste e implementação do metodo checkout e introdução aos snapshots

--- Modulo1/Projeto1/src/lib/cart.spec.js
+++ Modulo1/Projeto1/src/lib/cart.spec.js
@@ -1,52 +1,72 @@
 import Cart from './cart';
 
+let cart;
+
+let product = {
+  title: 'shoes - men',
+  price: '35388', //355.88 | R$383,88
+};
+
+let product2 = {
+  title: 'shoes - women',
+  price: '41872', //355.88 | R$383,88
+};
+
+beforeEach(() => {
+  cart = new Cart();
+});
+
 describe('Cart', () => {
-  let cart;
-
-  let product = {
-    title: 'shoes - men',
-    price: '35388', //355.88 | R$383,88
-  };
-
-  let product2 = {
-    title: 'shoes - women',
-    price: '41872', //355.88 | R$383,88
-  };
-
-  beforeEach(() => {
-    cart = new Cart();
-  });
-
-  it('should return 0 when getTotal is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
-  });
-
-  it('should multiply quantity and price and recieve the total amount', () => {
-    const item = {
-      product,
-      quantity: 2,
-    };
-
-    cart.add(item);
-
-    expect(cart.getTotal()).toEqual(70776);
-  });
-
-  it('should ensure no more than one procuct exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2,
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1,
+    it('should multiply quantity and price and recieve the total amount', () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
     });
 
-    expect(cart.getTotal()).toEqual(35388);
-  });
+    it('should ensure no more than one procuct exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
 
-  it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    });
+  });
+});
+
+describe('cehckout()', () => {
+  it('should return an object with the total and the list of items', () => {
     cart.add({
       product,
       quantity: 2,
@@ -54,11 +74,9 @@ describe('Cart', () => {
 
     cart.add({
       product: product2,
-      quantity: 1,
+      quantity: 3,
     });
 
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(41872);
+    expect(cart.checkout()).toMatchSnapshot();
   });
 });
