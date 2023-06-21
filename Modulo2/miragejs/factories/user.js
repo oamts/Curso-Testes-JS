b05ab63b8cb1e6cbc65ef7d64fbb915181384002
Mateus <mateusoliveiraalmeida@gmail.com>
2023-06-21 17:56:15 -0300

    Aula 7 - MirageJS Starter Kit

---
+++ Modulo2/miragejs/factories/user.js
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker';
+import { Factory } from 'miragejs';
+
+import { randomNumber } from './utils';
+
+export default {
+  user: Factory.extend({
+    name() {
+      return faker.person.fullName();
+    },
+    mobile() {
+      return faker.helpers.unique(faker.phone.number);
+    },
+    afterCreate(user, server) {
+      const messages = server.createList('message', randomNumber(10), { user });
+
+      user.update({ messages });
+    },
+  }),
+};
