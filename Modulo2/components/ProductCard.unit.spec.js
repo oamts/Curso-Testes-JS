fe0b5263faf398cd78cb33ef6413aa3b864dc988
Mateus <mateusoliveiraalmeida@gmail.com>
2023-06-22 18:29:56 -0300

    Aula 12 - Implementar AddToCart com TDD

--- Modulo2/components/ProductCard.unit.spec.js
+++ Modulo2/components/ProductCard.unit.spec.js
@@ -5,6 +5,23 @@ import { makeServer } from '@/miragejs/server';
 describe('ProductCard - unit', () => {
   let server;
 
+  const mountProductCard = () => {
+    const product = server.create('product', {
+      title: 'Relógio bonito',
+      price: '$23.00',
+      image:
+        'https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    });
+    return {
+      wrapper: mount(ProductCard, {
+        propsData: {
+          product,
+        },
+      }),
+      product,
+    };
+  };
+
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
   });
@@ -13,37 +30,27 @@ describe('ProductCard - unit', () => {
     server.shutdown();
   });
 
-  afterEach(() => {
-    server.shutdown();
-  });
-
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '23.00',
-          image:
-            'https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-        }),
-      },
-    });
+    const { wrapper } = mountProductCard();
 
     expect(wrapper.element).toMatchSnapshot();
   });
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-        }),
-      },
-    });
+    const { wrapper } = mountProductCard();
 
     expect(wrapper.vm).toBeDefined();
     expect(wrapper.text()).toContain('Relógio bonito');
-    expect(wrapper.text()).toContain('22.00');
+    expect(wrapper.text()).toContain('$23.00');
+  });
+
+  it('should emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard();
+
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted().addToCart).toBeTruthy();
+    expect(wrapper.emitted().addToCart.length).toBe(1);
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
   });
 });
