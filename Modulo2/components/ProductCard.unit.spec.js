b6e17ec8f5d03d3f2f55e6865e0813918621cbdd
Mateus <mateusoliveiraalmeida@gmail.com>
2023-07-04 15:59:57 -0300

    Aula 32 - Corrigir teste após refatorar componente

--- Modulo2/components/ProductCard.unit.spec.js
+++ Modulo2/components/ProductCard.unit.spec.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
 import { makeServer } from '@/miragejs/server';
+import { cartState } from '@/state';
 
 describe('ProductCard - unit', () => {
   let server;
@@ -44,13 +45,13 @@ describe('ProductCard - unit', () => {
     expect(wrapper.text()).toContain('$23.00');
   });
 
-  it('should emit the event addToCart with product object when button gets clicked', async () => {
-    const { wrapper, product } = mountProductCard();
+  it('should add item to cartState on button click', async () => {
+    const { wrapper } = mountProductCard();
 
     await wrapper.find('button').trigger('click');
 
-    expect(wrapper.emitted().addToCart).toBeTruthy();
-    expect(wrapper.emitted().addToCart.length).toBe(1);
-    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
+    expect(cartState.items).toHaveLength(1);
   });
+
+  it.todo('should ensure product is not added to the cart twice');
 });
