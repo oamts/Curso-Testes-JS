13ec845792a638cfccb67658831f701d9140ce6f
Mateus <mateusoliveiraalmeida@gmail.com>
2023-07-05 16:39:29 -0300

    Aula 48 - Testar busca por produtos

--- Modulo2/cypress/e2e/Store.spec.js
+++ Modulo2/cypress/e2e/Store.spec.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 import { makeServer } from '../../miragejs/server';
 
 context('Store', () => {
@@ -16,5 +17,30 @@ context('Store', () => {
 
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
+  });
+
+  context('Store > Search for Products', () => {
+    it('should type in the search field', () => {
+      cy.visit('http://localhost:3000');
+
+      cy.get('input[type="search"]')
+        .type('Some text here')
+        .should('have.value', 'Some text here');
+    });
+
+    it.only('should type in the search field', () => {
+      server.createList('product', 1, {
+        title: 'Relógio Bonito',
+      });
+      server.createList('product', 10);
+
+      cy.visit('http://localhost:3000');
+
+      cy.get('input[type="search"]').type('Relógio Bonito');
+
+      cy.get('[data-testid="search-form"]').submit();
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
+    });
   });
 });
