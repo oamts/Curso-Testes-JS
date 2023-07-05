6c19a5805bb36d1bf745f39c9d86552b0ae69583
Mateus <mateusoliveiraalmeida@gmail.com>
2023-07-05 16:44:46 -0300

    Aula 49 - Finalizando os testes para busca

--- Modulo2/cypress/e2e/Store.spec.js
+++ Modulo2/cypress/e2e/Store.spec.js
@@ -28,19 +28,26 @@ context('Store', () => {
         .should('have.value', 'Some text here');
     });
 
-    it.only('should type in the search field', () => {
+    it('should return 1 product when on specific search', () => {
       server.createList('product', 1, {
         title: 'Relógio Bonito',
       });
       server.createList('product', 10);
 
       cy.visit('http://localhost:3000');
-
       cy.get('input[type="search"]').type('Relógio Bonito');
-
       cy.get('[data-testid="search-form"]').submit();
-
       cy.get('[data-testid="product-card"]').should('have.length', 1);
+    });
+
+    it('should not return any product', () => {
+      server.createList('product', 10);
+
+      cy.visit('http://localhost:3000');
+      cy.get('input[type="search"]').type('Relógio Bonito');
+      cy.get('[data-testid="search-form"]').submit();
+      cy.get('[data-testid="product-card"]').should('have.length', 0);
+      cy.get('body').contains('0 Products');
     });
   });
 });
