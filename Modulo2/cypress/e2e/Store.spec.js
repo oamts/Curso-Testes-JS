21d0b3faeaf6df699c5a8e11fa9c9f01211c9b63
Mateus <mateusoliveiraalmeida@gmail.com>
2023-07-05 17:56:43 -0300

    Aula 50 - Testar Product List

--- Modulo2/cypress/e2e/Store.spec.js
+++ Modulo2/cypress/e2e/Store.spec.js
@@ -13,15 +13,42 @@ context('Store', () => {
   });
 
   it('should display the store', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
 
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
   });
 
+  context('Store > Product List', () => {
+    it('should display "0 products" when no product is returned', () => {
+      cy.visit('/');
+
+      cy.get('[data-testid="product-card"]').should('have.length', 0);
+      cy.get('body').contains('0 Products');
+    });
+
+    it('should display "1 products" when 1 product is returned', () => {
+      server.create('product');
+
+      cy.visit('/');
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
+      cy.get('body').contains('1 Product');
+    });
+
+    it('should display "10 products" when 10 product are returned', () => {
+      server.createList('product', 10);
+
+      cy.visit('/');
+
+      cy.get('[data-testid="product-card"]').should('have.length', 10);
+      cy.get('body').contains('10 Products');
+    });
+  });
+
   context('Store > Search for Products', () => {
     it('should type in the search field', () => {
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
 
       cy.get('input[type="search"]')
         .type('Some text here')
@@ -34,7 +61,7 @@ context('Store', () => {
       });
       server.createList('product', 10);
 
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
       cy.get('input[type="search"]').type('Relógio Bonito');
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should('have.length', 1);
@@ -43,7 +70,7 @@ context('Store', () => {
     it('should not return any product', () => {
       server.createList('product', 10);
 
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
       cy.get('input[type="search"]').type('Relógio Bonito');
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should('have.length', 0);
