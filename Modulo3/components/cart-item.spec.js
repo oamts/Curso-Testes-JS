6ae7754f408afe3a0cbf81f365f4645eb0ede0db
Mateus <mateusoliveiraalmeida@gmail.com>
2023-07-10 15:58:44 -0300

    Aula 8 - Testar componente CartItem - parte 1

---
+++ Modulo3/components/cart-item.spec.js
@@ -0,0 +1,34 @@
+import CartIem from './cart-item';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+const product = {
+  title: 'Relógio bonito',
+  price: '22.00',
+  image:
+    'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+};
+
+const renderCartItem = () => render(<CartIem product={product} />);
+
+describe('CartIem', () => {
+  it('should render ProductCard', function () {
+    renderCartItem();
+
+    expect(screen.getByTestId('cart-item')).toBeInTheDocument();
+  });
+
+  it('should display proper content', () => {
+    renderCartItem();
+
+    const image = screen.getByTestId('image');
+
+    expect(
+      screen.getByText(new RegExp(product.title, 'i')),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(new RegExp(product.price, 'i')),
+    ).toBeInTheDocument();
+    expect(image).toHaveProperty('src', product.image);
+    expect(image).toHaveProperty('alt', product.title);
+  });
+});
