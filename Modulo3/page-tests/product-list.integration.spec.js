cfb37e2b8e60a3dd861152a167683fc7b5d94f2f
Mateus <mateusoliveiraalmeida@gmail.com>
2023-07-10 21:18:58 -0300

    Aula 13 - Integration Tests - parte 2

--- Modulo3/page-tests/product-list.integration.spec.js
+++ Modulo3/page-tests/product-list.integration.spec.js
@@ -1,10 +1,29 @@
 import ProductList from '../pages';
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
+
+const renderProductList = () => {
+  render(<ProductList />);
+};
 
 describe('ProductList', function () {
   it('should render ProductList', function () {
-    render(<ProductList />);
+    renderProductList();
 
     expect(screen.getByTestId('product-list')).toBeInTheDocument();
   });
+
+  fit('should render the ProductCard component 10 times', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(10);
+    });
+  });
+
+  it.todo('should render the no products message');
+  it.todo('should render the Search component');
+  it.todo('should filter the product list when a search is performed');
+  it.todo('should display error message when promise rejects');
+  it.todo('should display the total quantity of products');
+  it.todo('should display product (singular) when there is only 1 product');
 });
